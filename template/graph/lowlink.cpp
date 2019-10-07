1fc5890c1e2fd38af3d54d7cf5d5b5db5b619146
obatakyoukan <oba-kyoukan@eis.hokudai.ac.jp>
2019-10-08 01:20:20 +0900

    -a

--- template/graph/lowlink.cpp
+++ template/graph/lowlink.cpp
@@ -1,55 +1,68 @@
-﻿#include<bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-// 関節点、橋、lowlink
-// lowlink
-typedef pair<int,int> mypair;
-void dfs(int v,int p,int &k,vector<int> &ord,vector<int> &low,vector<bool> &used,vector<vector<int> > &g,vector<mypair> &bridge,vector<int> &articulation){
+typedef long long int ll;
+typedef pair<ll,ll> mp;
+ll inf =  1e9;
+
+struct Lowlink{//O(v) ??? O(v + e )???
+    ll n;
+    vector<vector<ll> > g;
+    vector< mp > bridge;
+    vector< ll > articulation;
+    vector< ll > ord , low;
+    vector< bool > used;
+    Lowlink(){}
+    Lowlink( ll n ):n(n){
+	g = vector<vector<ll> >(n);
+	ord = vector<ll>(n);
+	low = vector<ll>(n);
+	used = vector<bool>(n,false);
+    }
+    void add_edge( ll s , ll t ){
+	g[s].push_back( t );
+	g[t].push_back( s );
+    }
+    void solve(){
+	ll k = 0;
+	for(ll i=0;i<n;i++)if( !used[i] )dfs( i , -1 , k );
+	sort( bridge.begin(),bridge.end() );
+	sort( articulation.begin(),articulation.end() );
+    }
+    void dfs(ll v, ll p ,ll &k){
 	used[v] = true;
 	ord[v] = k++;
 	low[v] = ord[v];
 	bool isarticulation = false;
-	int cnt = 0;
-	for(int i=0;i<g[v].size();i++){
-		if(!used[g[v][i]]){
-			cnt++;
-			dfs(g[v][i],v,k,ord,low,used,g,bridge,articulation);
-			low[v] = min(low[v],low[g[v][i]]);
-			if(~p&&ord[v]<=low[g[v][i]])isarticulation = true;
-			if(ord[v]<low[g[v][i]])bridge.push_back( mypair( min(v,g[v][i]), max(v,g[v][i]) ) ) ;
-		}
-		else if(g[v][i] != p){
-			low[v] = min(low[v],ord[g[v][i]]);
-		}
+	ll cnt = 0;
+	for(ll i=0;i<g[v].size();i++){
+	    if( !used[ g[v][i] ] ){
+		cnt++;
+		dfs(g[v][i],v,k);
+		low[v] = min( low[v] , low[ g[v][i] ] );
+		if( ~p && ord[v] <= low[g[v][i]])isarticulation = true;
+		if( ord[v] < low[ g[v][i] ] )bridge.push_back( mp( min( v, g[v][i] ) , max( v, g[v][i] ) ) );
+	    }else if( g[v][i] != p ){
+		low[v] = min( low[v] , ord[ g[v][i] ] );
+	    }
 	}
-	if(p==-1 &&cnt>1)isarticulation=true;
-	if(isarticulation)articulation.push_back(v);
-}
+	if( p==-1 && cnt > 1 )isarticulation = true;
+	if( isarticulation ) articulation.push_back( v );
+    }
+};
 
 int main(){
-	int v,e;
-	cin>>v>>e;
-	vector<vector<int> >g(v);
-	vector<int> ord(v),low(v);
-	vector<int> art;
-	vector<mypair> bridge;
-	vector<bool> used(v,false);
-	int k=0;
-
-	for(int i=0;i<e;i++){
-	int s,t;
+    ll v,e;
+    cin>>v>>e;
+    Lowlink L(v);
+    for(ll i=0;i<e;i++){
+	ll s,t;
 	cin>>s>>t;
-	g[s].push_back(t);
-	g[t].push_back(s);
-	}
-
-	for(int i=0;i<v;i++)if(!used[i])dfs(i,-1,k,ord,low,used,g,bridge,art);
-	sort(art.begin(),art.end());
-	sort(bridge.begin(),bridge.end());
-	//関節点
-	cout<<art.size()<<endl;
-	for(int i=0;i<art.size();i++)cout<<art[i]<<endl;	
-	//橋
-	cout<<bridge.size()<<endl;
-	for(int i=0;i<bridge.size();i++)cout<<bridge[i].first<<' '<<bridge[i].second<<endl;
-	return 0;
+	L.add_edge( s,t );
+    }
+    L.solve();
+    for(auto i:L.bridge ){
+	cout<<i.first<<' '<<i.second<<endl;
+    }
+    return 0;
 }
+
